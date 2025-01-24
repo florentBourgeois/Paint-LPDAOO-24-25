d4d85ea699c6d0cd02b7bf06197339f3c2b85997
Florent.Bourgeois <florent.bourgeois@uha.fr>
2025-01-24 23:04:46 +0100

    ajout classes et pdf

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,31 +1,44 @@
 public class Rectangle extends Forme{
 
     private int longueur = 5;
-    private int largeur;
+    private int hauteur;
 
     public Rectangle() {
-        this.largeur = 10;
+        this.hauteur = 10;
         this.longueur = 5;
     }
 
-    public Rectangle(int largeur, int longueur) {
-        this.largeur = largeur;
+    public Rectangle(int hauteur, int longueur) {
+        this.hauteur = hauteur;
         this.longueur = longueur;
     }
 
-    public Rectangle(int largeur, int longueur, int x, int y) {
+    public Rectangle(int hauteur, int longueur, int x, int y) {
         super(x,y);
         this.longueur = longueur;
-        this.largeur = largeur;
+        this.hauteur = hauteur;
     }
 
     public void setLongueur(int longueur) {
         this.longueur = longueur;
     }
 
+    public int getPerimetre(){
+        return (this.hauteur + this.longueur)*2;
+    }
+
+    public String toHTMLCanvas(){
+        String res = "ctx.fillRect(";
+        res += this.getX() + ",";
+        res += this.getY() + ",";
+        res += this.hauteur + ",";
+        res += this.longueur + ");";
+        return res;
+    }
+
     @Override
     public String toString() {
-        return "Je suis un rectangle avec une longueur de : " + this.longueur + " et une largeur " + this.largeur + " le perimetre = " + this.getPerimetre() + "la position " + super.toString();
+        return "Je suis un rectangle avec une longueur de : " + this.longueur + " et une hauteur " + this.hauteur + " le perimetre = " + this.getPerimetre() + "la position " + super.toString();
     }
 
 
@@ -38,22 +51,7 @@ public class Rectangle extends Forme{
         System.out.println(r2);
     }
 
-    public int getLongueur() {
-        return this.longueur;
-    }
 
-    public int getPerimetre(){
-        return (this.largeur + this.longueur)*2;
-    }
-
-    public String toHTMLCanvas(){
-        String res = "ctx.fillRect(";
-        res += this.getX() + ",";
-        res += this.getY() + ",";
-        res += this.largeur + ",";
-        res += this.longueur + ");";
-        return res;
-    }
 
 
 }
