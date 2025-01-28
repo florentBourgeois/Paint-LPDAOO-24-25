224f8f7d2b8cfee39a35a578070d07cff87c4e32
Florent.Bourgeois <florent.bourgeois@uha.fr>
2025-01-28 10:05:18 +0100

    ajout du paragraphe

---
+++ src/pageweb/Paragraph.java
@@ -0,0 +1,23 @@
+package pageweb;
+
+public class Paragraph {
+
+    String text = "Hello world";
+
+    Paragraph(){}
+
+    Paragraph(String textDuParagraph){
+        this.text = textDuParagraph;
+    }
+
+    public String toHTML(){
+        return "<p>" + this.text + "</p>";
+    }
+
+    @Override
+    public String toString() {
+        return "Paragraph{" +
+                "text='" + text + '\'' +
+                '}';
+    }
+}
