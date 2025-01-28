439b740c0e59fff1981be183ff2c98ceadcf3f37
Florent.Bourgeois <florent.bourgeois@uha.fr>
2025-01-28 13:13:29 +0100

    ajouter des tags

--- src/pageweb/PageWeb.java
+++ src/pageweb/PageWeb.java
@@ -8,13 +8,46 @@ public class PageWeb {
     private final String footer = "</body>";
     private List<Tag> tags = new ArrayList<>();
 
+    public PageWeb() {
+    }
+
+    PageWeb(List<Tag> tags){
+        this.tags = tags;
+    }
+
+    public void addTag(Tag t){
+        this.tags.add(t);
+    }
+
+
     public String toHTML(){
         String res = "";
         for (Tag t : tags){
             res += "\n\t" + t.toHTML();
         }
-
         return header + res + footer;
     }
 
+
+    public static void main(String[] args) {
+        PageWeb page = new PageWeb();
+        System.out.println(page.toHTML());
+
+        Paragraph p = new Paragraph();
+        page.addTag(p);
+        System.out.println(page.toHTML());
+
+        page.addTag(new Paragraph("le second paragraph"));
+        page.addTag(new Paragraph("le troisieme paragraph"));
+        System.out.println("-----plusieurs paragraphes---------");
+        System.out.println(page.toHTML());
+        page.addTag(new Title());
+        page.addTag(new Img());
+        page.addTag(new Link());
+        System.out.println("-----plusieurs tags---------");
+        System.out.println(page.toHTML());
+
+
+
+    }
 }
